fa456418d770b5e73e99ef1cfb554743cb7070e0
Vishal P <118256717+Vishalvishal-2005@users.noreply.github.com>
2025-02-12 19:37:03 +0530

    Update Venue.java

--- src/main/java/com/example/demo/model/Venue.java
+++ src/main/java/com/example/demo/model/Venue.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
+@Table(name = "venue")
+
 public class Venue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
