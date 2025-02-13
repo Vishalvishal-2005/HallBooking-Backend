ea791e5c217184038641ce88c432ccb87b830f9a
Vishal P <118256717+Vishalvishal-2005@users.noreply.github.com>
2025-02-13 09:43:39 +0530

    Update CorsConfig.java

--- src/main/java/com/example/demo/configuration/CorsConfig.java
+++ src/main/java/com/example/demo/configuration/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
-  @Bean
+@Bean
 public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
         @Override
